8eac8c685a08b5162de89881c752b79e2e0e2497
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-07-27 17:40:30 +0000

    Update unicorn config to listen on NGINX socket.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,13 @@
-rails_root = File.expand_path('../../', __FILE__)
-worker_processes 2
-working_directory rails_root
-
-listen "#{rails_root}/tmp/unicorn.sock"
-pid "#{rails_root}/tmp/unicorn.pid"
-
-stderr_path "#{rails_root}/log/unicorn_error.log"
-stdout_path "#{rails_root}/log/unicorn.log"
-
+# rails_root = File.expand_path('../../', __FILE__)
+# worker_processes 2
+# working_directory rails_root
+#
+# listen "#{rails_root}/tmp/unicorn.sock"
+# pid "#{rails_root}/tmp/unicorn.pid"
+#
+# stderr_path "#{rails_root}/log/unicorn_error.log"
+# stdout_path "#{rails_root}/log/unicorn.log"
+#
 
 require 'fileutils'
 listen '/tmp/nginx.socket'
